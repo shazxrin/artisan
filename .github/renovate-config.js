b2ee67030a041ce59ab2d3f3b2959d3ebd2340c2
shazrin <mshazrinbs@gmail.com>
2025-06-26 15:20:58 +0800

    ci: add renovate workflow

---
+++ .github/renovate-config.js
@@ -0,0 +1,42 @@
+modules.exports = {
+    repositories: [
+        "shazxrin/notifier",
+        "shazxrin/one-percent-better"
+    ],
+    extends: [
+        "config:recommended"
+    ],
+    platform: "github",
+    labels: [
+        "dependencies"
+    ],
+    automerge: false,
+    packageRules: [
+        {
+            matchManagers: [
+                "gradle"
+            ],
+            matchDepTypes: [
+                "dependencies",
+                "pluginDependencies",
+                "dependencyManagement",
+                "plugins"
+            ],
+            enabled: true,
+        },
+        {
+            matchManagers: [
+                "github-actions"
+            ],
+            enabled: true
+        }
+    ],
+    hostRules: [
+        {
+            matchHost: "https://maven.pkg.github.com/shazxrin",
+            hostType: "maven",
+            token: process.env.RENOVATE_TOKEN
+        }
+    ],
+    ignoreDeps: []
+}
